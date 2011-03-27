b7a240157c83496310ea82621c33c8920cdc76e6
luzi82 <luzi82@gmail.com>
2011-03-27 09:52:49 +0800

    Change start game to StartGameMenuActivity

--- src/com/luzi82/iconmatch/SelectGameDirectionActivity.java
+++
@@ -1,7 +0,0 @@
-package com.luzi82.iconmatch;
-
-import android.app.ListActivity;
-
-public class SelectGameDirectionActivity extends ListActivity {
-
-}
---
+++ src/com/luzi82/iconmatch/StartGameMenuActivity.java
@@ -0,0 +1,33 @@
+package com.luzi82.iconmatch;
+
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+
+public class StartGameMenuActivity extends PreferenceActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		// setContentView(R.layout.simplelist);
+		// initList();
+	}
+
+	// @Override
+	// protected void onListItemClick(ListView l, View v, int position, long id)
+	// {
+	// super.onListItemClick(l, v, position, id);
+	// }
+	//
+	// private void initList() {
+	// List<String> idList = new ArrayList<String>();
+	// idList.add("Color");
+	//
+	// ArrayAdapter<String> aa = new ArrayAdapter<String>(this,
+	// android.R.layout.simple_list_item_1, idList);
+	// setListAdapter(aa);
+	//
+	// onContentChanged();
+	// }
+
+}
