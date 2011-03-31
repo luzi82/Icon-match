8d5e032321a96ed32a385eac7836f7ceee9e38a7
luzi82 <luzi82@gmail.com>
2011-04-01 00:19:33 +0800

    add end game logic

--- src/com/luzi82/iconmatch/IconMatchGame.java
+++ src/com/luzi82/iconmatch/IconMatchGame.java
@@ -2,6 +2,7 @@ package com.luzi82.iconmatch;
 
 import java.util.LinkedList;
 
+import android.app.Activity;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
@@ -16,9 +17,11 @@ public class IconMatchGame extends AbstractGame {
 	final static float LIFE_MAX_UNIT = 100;
 	final static float LIFE_MIN_UNIT = 0;
 
-	final static Paint LINE_PAINT = new Paint();
+	final static Paint LIVE_PAINT = new Paint();
+	final static Paint DEAD_PAINT = new Paint();
 	static {
-		LINE_PAINT.setColor(Color.WHITE);
+		LIVE_PAINT.setColor(Color.WHITE);
+		DEAD_PAINT.setColor(Color.RED);
 	}
 
 	long mStartTime = System.currentTimeMillis();
@@ -31,19 +34,22 @@ public class IconMatchGame extends AbstractGame {
 
 	float mLifeUnit = LIFE_MAX_UNIT; // 0-100
 
+	boolean mGameEnd = false;
+	long mGameEndStartMs;
+
 	LinkedList<Integer> answer = new LinkedList<Integer>();
 
 	@Override
 	public void draw(Canvas c) {
+		Paint paint = mGameEnd ? DEAD_PAINT : LIVE_PAINT;
 		c.drawColor(Color.BLACK);
 		float lineY = mScreenHeight - (mLifeUnit * mBarScreenHeight / BAR_UNIT);
 		for (int v : answer) {
-			float topY = lineY - mBarScreenHeight+1;
+			float topY = lineY - mBarScreenHeight + 1;
 			if (v == 0) {
-				c.drawRect(0, topY, mScreenWidth / 2, lineY, LINE_PAINT);
+				c.drawRect(0, topY, mScreenWidth / 2, lineY, paint);
 			} else {
-				c.drawRect(mScreenWidth / 2, topY, mScreenWidth, lineY,
-						LINE_PAINT);
+				c.drawRect(mScreenWidth / 2, topY, mScreenWidth, lineY, paint);
 			}
 			lineY -= mBarScreenHeight;
 		}
@@ -57,13 +63,26 @@ public class IconMatchGame extends AbstractGame {
 
 	@Override
 	public void tick() {
-		long timeDiff = System.currentTimeMillis() - mStartTime;
-		float speed = BAR_UNIT * (mIconMatchGameActivity.getPeriodMs())
-				* (float) Math.pow(2.0d, ((double) (timeDiff) / 60000))
-				/ 1000.0f;
-		mLifeUnit -= speed;
-		while (answer.size() < mScreenBarCount) {
-			answer.addLast((Math.random() > 0.5) ? 1 : 0);
+		long now = System.currentTimeMillis();
+		if (!mGameEnd) {
+			long timeDiff = System.currentTimeMillis() - mStartTime;
+			float speed = BAR_UNIT * (mIconMatchGameActivity.getPeriodMs())
+					* (float) Math.pow(2.0d, ((double) (timeDiff) / 60000))
+					/ 1000.0f;
+			mLifeUnit -= speed;
+			while (answer.size() < mScreenBarCount) {
+				answer.addLast((Math.random() < 0.5) ? 0 : 1);
+			}
+			if (mLifeUnit <= 0) {
+				mLifeUnit = 0;
+				mGameEnd = true;
+				mGameEndStartMs = now;
+			}
+		} else {
+			if (now - mGameEndStartMs >= 2000) {
+				// mIconMatchGameActivity.setResult(Activity.RESULT_OK);
+				mIconMatchGameActivity.finish();
+			}
 		}
 	}
 
@@ -77,12 +96,21 @@ public class IconMatchGame extends AbstractGame {
 
 	@Override
 	public void onTouchEvent(MotionEvent event) {
-		if (event.getAction() == MotionEvent.ACTION_DOWN) {
-			mLifeUnit += BAR_UNIT;
-			if (mLifeUnit > LIFE_MAX_UNIT) {
-				mLifeUnit = LIFE_MAX_UNIT;
+		if (!mGameEnd) {
+			if (event.getAction() == MotionEvent.ACTION_DOWN) {
+				int v = (event.getX() < mScreenWidth / 2) ? 0 : 1;
+				int first = answer.getFirst();
+				if (first == v) {
+					mLifeUnit += BAR_UNIT;
+					if (mLifeUnit > LIFE_MAX_UNIT) {
+						mLifeUnit = LIFE_MAX_UNIT;
+					}
+					answer.removeFirst();
+				} else {
+					mGameEnd = true;
+					mGameEndStartMs = System.currentTimeMillis();
+				}
 			}
-			answer.removeFirst();
 		}
 	}
 
@@ -94,7 +122,7 @@ public class IconMatchGame extends AbstractGame {
 	public void surfaceChanged(int format, int width, int height) {
 		mScreenWidth = width;
 		mScreenHeight = height;
-		mBarScreenHeight = width / 3;
+		mBarScreenHeight = width / 4;
 		mScreenBarCount = (mScreenHeight / mBarScreenHeight) + 1;
 	}
 
