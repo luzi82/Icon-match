5a7eff62a041e33bdca8ec0f7966becaec9c8e85
luzi82 <luzi82@gmail.com>
2011-04-10 18:22:24 +0800

    move i18n.ods to src2 folder (src square, src of src)

--- build.mk
+++ build.mk
@@ -5,12 +5,12 @@ TOOL_I18N_PATH = ${TOOL_PATH}/i18n
 
 all : .i18n_timestamp
 
-.i18n_timestamp : i18n.ods
+.i18n_timestamp : src2/i18n/i18n.ods
 	java \
 		-jar ${TOOL_I18N_PATH}/CodeTemplate.jar \
 		-g ${TOOL_I18N_PATH}/code_template_global_config.xml \
 		-t ${TOOL_I18N_PATH}/ods2xml.xml \
-		iconmatch_loc_strings
+		src2/i18n/i18n.ods iconmatch_loc_strings
 	touch .i18n_timestamp
 
 clean :
